9de8e080c005c26d389c7c747fa4ec8e51af054c
knigawkl <lukasz.knigawka@outlook.com>
2018-12-15 19:53:12 +0100

    Adding project icon, window background, drawing points==rectangles

---
+++ LUPA/LUPA/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LUPA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Point start;
+
+        private void Map_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            start = e.GetPosition(this);
+
+            var rec = new Rectangle()
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 5
+            };
+
+            Map.Children.Add(rec);
+            Canvas.SetTop(rec, start.Y);
+            Canvas.SetLeft(rec, start.X);
+
+        }
+    }
+}
---
+++ LUPA/LUPA/Parser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LUPA
+{
+    public class Parser
+    {
+        public struct CountourPoint
+        {
+            public int Id { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public List<CountourPoint> countour;
+        public int width = 0;
+        public int height = 0;
+
+        public void Parse(string input)
+        {
+            try
+            {
+                var lines = new List<string>();
+                using (var reader = new StreamReader(input))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+
+                int hashCounter = 0;
+                foreach (var line in lines)
+                {
+                    if (line.StartsWith("#"))
+                    {
+                        hashCounter++;
+                    }
+                }
+                if (hashCounter != 4)
+                {
+                    throw new Exception("There should be four comment lines, each starting with a hash symbol. " +
+                        "Currently there are " + hashCounter + " such lines");
+                }
+
+            }
+            catch (Exception e)
+            {
+                //wypisac do konsolki w okienku blad
+            }
+
+        }
+    }
+}
+
