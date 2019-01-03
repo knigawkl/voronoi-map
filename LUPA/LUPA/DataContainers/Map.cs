d4cb9e2143ee87b237b1a8e9a23a07c601a2fadc
smolinm1 <matsmolinski@gmail.com>
2019-01-03 23:18:50 +0100

    Implementation: structure of Parser, ParseFile method

---
+++ LUPA/LUPA/DataContainers/Map.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LUPA.DataContainers
+{
+    class Map
+    {
+        public List<Point> ContourPoints { set; get; }
+        public List<KeyPoint> KeyPoints { set; get; }
+        public List<LineSegment> AreaLineSegments { set; get; }
+        public List<CustomObjectType> CustomObjectTypes { set; get; }
+        public List<CustomObjectInstance> CustomObjects { set; get; }
+
+        public Map()
+        {
+            ContourPoints = new List<Point>();
+            KeyPoints = new List<KeyPoint>();
+            AreaLineSegments = new List<LineSegment>();
+            CustomObjectTypes = new List<CustomObjectType>();
+            CustomObjects = new List<CustomObjectInstance>();
+        }
+    }
+}
---
+++ LUPA/LUPA/ParserM.cs
@@ -0,0 +1,130 @@
+﻿using LUPA.DataContainers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LUPA
+{
+    class ParserM
+    {
+        private enum ParserState
+        {
+            START, CONTOURPOINTS, KEYPOINTS, OBJECTSDEF, OBJECTS
+        }
+
+        public Map ParseFile(string inputFilePath)
+        {
+            ParserState state = ParserState.START;
+            int lineCounter = 0;
+            Map map = new Map();
+            var reader = new StreamReader(inputFilePath);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                lineCounter++;
+                switch (state)
+                {
+                    case ParserState.START:
+                        if (line.Length > 0 && line[0] == '#')
+                        {
+                            state = ParserState.CONTOURPOINTS;                          
+                        }
+                        break;
+                    case ParserState.CONTOURPOINTS:
+                        if (line.Length > 0 && line[0] == '#')
+                        {
+                            state = ParserState.KEYPOINTS;
+                        }
+                        else
+                        {
+                            try
+                            {
+                                map.ContourPoints.Add(ParseContourPoint(line));
+                            }
+                            catch (Exception e)
+                            {
+                                throw new Exception(e.Message + " in line " + lineCounter);
+                            }
+                        }
+                        break;
+                    case ParserState.KEYPOINTS:
+                        if (line.Length > 0 && line[0] == '#')
+                        {
+                            state = ParserState.OBJECTSDEF;
+                        }
+                        else
+                        {
+                            try
+                            {
+                                map.KeyPoints.Add(ParseKeyPoint(line));
+                            }
+                            catch (Exception e)
+                            {
+                                throw new Exception(e.Message + " in line " + lineCounter);
+                            }
+                        }
+                        break;
+                    case ParserState.OBJECTSDEF:
+                        if (line.Length > 0 && line[0] == '#')
+                        {
+                            state = ParserState.OBJECTS;
+                        }
+                        else
+                        {
+                            try
+                            {
+                                map.CustomObjectTypes.Add(ParseCustomObjectType(line));
+                            }
+                            catch (Exception e)
+                            {
+                                throw new Exception(e.Message + " in line " + lineCounter);
+                            }
+                        }
+                        break;
+                    case ParserState.OBJECTS:
+                        if (line.Length > 0 && line[0] == '#')
+                        {
+                            throw new Exception("File contains more than four comment lines. There should be four lines starting with a hash symbol. Please verify the file.");
+                        }
+                        else
+                        {
+                            try
+                            {
+                                map.CustomObjects.Add(ParseCustomObject(line, map.CustomObjectTypes));
+                            }
+                            catch (Exception e)
+                            {
+                                throw new Exception(e.Message + " in line " + lineCounter);
+                            }
+                        }
+                        break;
+                }
+            }
+
+            return map;
+        }
+
+        private CustomObjectInstance ParseCustomObject(string line, List<CustomObjectType> customObjectTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+        private CustomObjectType ParseCustomObjectType(string line)
+        {
+            throw new NotImplementedException();
+        }
+
+        private KeyPoint ParseKeyPoint(string line)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Point ParseContourPoint(string line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
