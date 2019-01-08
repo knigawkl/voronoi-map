a5039d1b3a15f63737cdfeb8d417395b7a630123
knigawkl <lukasz.knigawka@outlook.com>
2019-01-08 01:44:50 +0100

    Adding adding and deleting shapes on the runtime

--- LUPA/LUPA/MainWindow.xaml.cs
+++ LUPA/LUPA/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -17,32 +16,48 @@ namespace LUPA
     public partial class MainWindow : Window
     {
         Map map = new Map();
-        System.Windows.Point start;
+        System.Windows.Point position;
 
         public MainWindow()
         {
             InitializeComponent();
         }
-      
+
+        /// <summary>
+        /// Draws a Key/Contour Point (depends on which radio button is checked)
+        /// </summary>
         private void Map_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            start = e.GetPosition(this);
-
-            var rec = new Rectangle()
+            position = e.GetPosition(this);
+            if (KeyPointBtn.IsChecked == true)
             {
-                Stroke = Brushes.BlueViolet,
-                StrokeThickness = 5
-            };
-
-            Map.Children.Add(rec);
-            Canvas.SetTop(rec, start.Y - TopToolbar.ActualHeight);
-            Canvas.SetLeft(rec, start.X);
+                DrawPoint(Brushes.IndianRed, position);
+                AddKeyPoint(position);
+            }
+            else if (ContourPointBtn.IsChecked == true)
+            {
+                DrawPoint(Brushes.LightSeaGreen, position);
+                AddContourPoint(position);
+            }
         }
-        /*
+
         private void Map_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if (e.OriginalSource is Shape clickedShape)
+            {
+                if (clickedShape.Stroke == Brushes.IndianRed && KeyPointBtn.IsChecked == true)
+                {
+                    Map.Children.Remove(clickedShape);
+                    //tu usunac z naszego map
+                    
+                }
+                if (clickedShape.Stroke == Brushes.LightSeaGreen && ContourPointBtn.IsChecked == true)
+                {
+                    Map.Children.Remove(clickedShape);
+                }
+            }
         }
-        */
+        
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
             var ofd = new OpenFileDialog
@@ -76,6 +91,40 @@ namespace LUPA
                 };
                 Map.Background = ib;
             }
+        }
+
+        private void KeyPointBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            KeyPointBtn.Background = Brushes.IndianRed;
+            ContourPointBtn.Background = Brushes.Azure;
+        }
+
+        private void ContourBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            ContourPointBtn.Background = Brushes.LightSeaGreen;
+            KeyPointBtn.Background = Brushes.Azure;
+        }
+
+        private void DrawPoint(Brush color, System.Windows.Point position)
+        {           
+            var rec = new Rectangle()
+            {
+                StrokeThickness = 5,
+                Stroke = color
+            };
+            Map.Children.Add(rec);
+            Canvas.SetTop(rec, position.Y - TopToolbar.ActualHeight);
+            Canvas.SetLeft(rec, position.X);
+        }
+
+        private void AddKeyPoint(System.Windows.Point position)
+        {
+            map.KeyPoints.Add(new KeyPoint(position.X, position.Y, "defaultKeyPointName"));
+        }
+
+        private void AddContourPoint(System.Windows.Point position)
+        {
+            map.ContourPoints.Add(new Point(position.X, position.Y));
         }
     }
 }
