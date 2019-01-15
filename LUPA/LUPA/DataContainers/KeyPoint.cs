608f58a2a3239252f9c2284cf2ed829340c7379b
knigawkl <lukasz.knigawka@outlook.com>
2019-01-15 15:08:22 +0100

    Adding colors to key points

--- LUPA/LUPA/DataContainers/KeyPoint.cs
+++ LUPA/LUPA/DataContainers/KeyPoint.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace LUPA
 {
     public class KeyPoint : Point
     {
         public string Name { set; get; }
+        public SolidColorBrush Color { set; get; } 
         public KeyPoint(double x, double y, string name) : base(x, y)
         {
             Name = name;
+            SetColor();
+        }
+
+        private void SetColor() {
+            SolidColorBrush result = Brushes.Transparent;
+
+            Random rnd = new Random();
+
+            Type brushesType = typeof(Brushes);
+
+            PropertyInfo[] properties = brushesType.GetProperties();
+
+            int random = rnd.Next(properties.Length);
+            result = (SolidColorBrush)properties[random].GetValue(null, null);
+
+            Color = result;
         }
     }
 }
